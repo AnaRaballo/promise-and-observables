ba1ea72b5df2ab39748f4230afe54142003ecbf8
sandrabosk <sandraboskovic@hotmail.com>
2018-03-13 11:12:08 -0400

    great to have

--- src/app/promise.service.ts
+++ src/app/promise.service.ts
@@ -11,6 +11,8 @@ export class PromiseService {
     return this.http.get('http://jsonplaceholder.typicode.com/posts') //hitting an external server with http request.
     .toPromise() //change return type from default observable<response> to promise<response> so that we can mimic a promise
     //asyn operation and implement promise operators
+    .then()
+    .catch()
   }
 
 }
